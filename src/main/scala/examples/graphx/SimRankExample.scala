e8dd4656f5df23e68b2bf7e5b633eef0b66b0751
alphajin1 <alphajin1@gmail.com>
2020-11-10 23:10:12 +0900

    temp commit :

--- src/main/scala/examples/graphx/SimRankExample.scala
+++ src/main/scala/examples/graphx/SimRankExample.scala
@@ -18,135 +18,105 @@ class Matrix() {
 }
 
 object SimRankExample {
-  def createGraph(edges: RDD[(String, String)]): Graph[String, String] = {
-    // vertices : originId, assignedId
-    val vertices = (edges.map(_._1) union (edges.map(_._2))).distinct.zipWithIndex()
-    //val relationships: RDD[Edge[String]]  = nods.join(indnode).map(x=>x._2).join(indnode).map(_._2).map { x =>
-    val relationships = edges.map { x =>
-      val x1 = (x._1 + "L").replace("L", "").toLong
-      val x2 = (x._2 + "L").replace("L", "").toLong
-      val ed = Edge(x1, x2, "1")
-      ed
-    }.groupBy {
-      x => (x.srcId, x.dstId) // srcId, dstId, attr
-    }.map {
-      x =>
-        // x._1 = (srcId, dstId), x._2 = CompactBuffer(List)
-        // set = srcId, dstId, weight(String, Length)
-        val set = Edge(x._1._1.toString.toLong, x._1._2.toString.toLong, x._2.toVector.length.toString)
-        set
-    }
-
-    val users = vertices.map(line => (line, "id")).map { x =>
-      val x1 = x._1._2 // x1 = assignedId
-      (x1, x._2) // (assignedId, id) pair
-    }
-
-    // id가 있으면, MappingId 이고, 아니면 OriginalId
-    val graph = Graph(users, relationships)
-    graph
-  }
 
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
-
+    val logger = Logger.getRootLogger()
     val (spark, sc) = MySparkSession.getDefault(s"${this.getClass.getSimpleName}")
-    val delta = 0.8
 
-    val filePath = "data/graphx/simrank_data.txt"
-    val file = sc.textFile(filePath)
-    val edges: RDD[(String, String)] = file.map { x =>
-      val raw_edges = (x.split("\t")(0), x.split("\t")(1))
-      raw_edges
-    }
+    // Create an RDD for the vertices
+    val vertices: RDD[(VertexId, (String, String))] =
+      sc.parallelize(Seq(
+        (1L, ("q", "Univ")),
+        (2L, ("q", "ProfA")),
+        (3L, ("d", "ProfB")),
+        (4L, ("d", "StudentA")),
+        (5L, ("d", "StudentB"))
+      ))
 
-    val vertices = (edges.map(_._1) union (edges.map(_._2))).distinct.zipWithIndex()
-    var numOfVertices = vertices.collect.length
-    // numOfVertices: 327
-    // numOfEdges : 6810
+    // Create an RDD for edges
+    val edges: RDD[Edge[Double]] =
+      sc.parallelize(Seq(
+        Edge(1L, 2L, 1.0),
+        Edge(1L, 3L, 1.0),
+        Edge(2L, 4L, 1.0),
+        Edge(4L, 1L, 1.0),
+        Edge(3L, 5L, 1.0),
+        Edge(5L, 3L, 1.0)
+      ))
 
-    val graph = createGraph(edges)
-    // outDegrees = VertexId, Count(OutDegrees)
-    // DEBUG : graph.edges.collect().filter(x => x.srcId == 5812) = 19개, 1/19로 변환
+    val aEdges = edges // .union(edges.map(x => Edge(x.dstId, x.srcId, x.attr)))
+    val graph = Graph(vertices, aEdges)
+
+    // graph.outDegrees : VertexId, Count(OutDegrees)
     val outDegrees = graph.outDegrees.map(x => (x._1, (1.toDouble / x._2.toDouble)))
     val normalizedEdges = graph.outerJoinVertices(outDegrees) {
-      // assignedId 이면 weight = 0
-      (id, _, weight) => (id.toString, weight.getOrElse(0))
-    }.triplets.map { x =>
-      // SourceId, DestId에 대해 x.srcAttr = Weight, x.attr = 1
-      val set = (x.srcId, x.dstId, x.srcAttr._2.toString.toDouble * x.attr.toInt)
+      // vertexId = JoinKey, keyAttr = id or null, joinAttr = weight
+      (vertexId, keyAttr, joinAttr) => (vertexId, joinAttr.getOrElse(0))
+    }.triplets.map {
+      x =>
+      // x = triplets
+      // SourceId, DestId에 대해 x.srcAttr = Weight, x.attr = 1, 즉 (srcId, dstId, weight)
+      val set = (x.srcId, x.dstId, x.srcAttr._2.toString.toDouble * x.attr)
       set
     }
 
-    // 6810
-    normalizedEdges.take(5).foreach(println)
-
-    //val sg = stgra//.map(_._1).union(stgra.map(_._2))
-    // 164
-    val indnode = (normalizedEdges.map(_._1).union(normalizedEdges.map(_._2))).distinct.zipWithIndex()
-    val sgnu = normalizedEdges.map(x => (x._1, (x._2, x._3))).join(indnode).map(x => (x._2._1._1, (x._2._1._2, x._2._2))).join(indnode).map(x => (x._2._1._2, x._2._2, x._2._1._1))
-    sgnu.take(5).foreach(println)
-    val sg = sgnu.map(x => (x._2, x._1, x._3)).union(sgnu.map(x => (x._1, x._2, 1.toDouble / numOfVertices)))
-
-    numOfVertices = sg.count.toInt
-    //    val inputGraph: Graph[PartitionID, String] = graph.outerJoinVertices(graph.outDegrees)((vid, _, degOpt) => degOpt.getOrElse(0))
-    //    // Construct a graph where each edge contains the weight
-    //    // and each vertex is the initial PageRank
-    //    val node_len = graph.triplets.map(trip => trip.srcId).union(graph.triplets.map(trip => trip.dstId)).distinct.count.toInt //获取所有节点总数
-    //
-    //    val m_list = new Matrix() //生成矩阵格式的List
-    //
-    //    //val node1 = gra.edges.map(x => x.relativeDirection(EdgeDirection.Either)).collect
-    //    //all the input neighbors
-    //    val maptab = graph.collectNeighborIds(EdgeDirection.Either).map { x =>
-    //      val set = (x._1, x._2.toVector, 1.toDouble / x._2.length.toDouble)
-    //      set
-    //    }
-
-    // 164 * 164
-    // 와 진짜 이거 계산을 어떻게 하란거지?
-    val matrixprob = new CoordinateMatrix(sg.map { x => //概率转移矩阵
-      val s = MatrixEntry(x._1, x._2, x._3)
-      s
+    val weightMatrix = new CoordinateMatrix(normalizedEdges.map {
+      x =>
+        MatrixEntry(x._1, x._2, x._3)
     })
 
-    numOfVertices = matrixprob.numRows.toInt
-    val matrixp = new CoordinateMatrix(matrixprob.entries.map { x =>
-      val set = MatrixEntry(x.i, x.i, 1.0)
-      set
-    })
-    matrixprob.entries.take(5).foreach(println)
-    //    val unit = new CoordinateMatrix(matrixp.entries.map { x => (x.i, x.i, x.value) }.union(matrixp.entries.map(x => (x.j, x.j, x.value))).map { x =>
-    //      val set = MatrixEntry(x._1, x._2, x._3 * delta)
-    //      set
-    //    }.distinct)
-    val In = new CoordinateMatrix(matrixp.entries.map { x => //s0 = (1-C)In
-      val set = MatrixEntry(x.i, x.j, x.value * (1 - delta))
-      set
+    val identityMatrix = new CoordinateMatrix(graph.vertices.map { x =>
+      MatrixEntry(x._1, x._1, 1.0)
     })
 
-    val S0 = In //初始化S0
-    var Sk = S0
-    var Skp1 = Sk
+    // 이게... 메트릭스가 돼??
+    val delta = 0.8
+    var tempMatrix = identityMatrix
+    var resultMatrix = tempMatrix
+    for (i <- 0 to 5) {
+      resultMatrix = new CoordinateMatrix(
+        weightMatrix.toBlockMatrix.transpose
+          .multiply(tempMatrix.toBlockMatrix)
+          .multiply(weightMatrix.toBlockMatrix)
+          .toCoordinateMatrix.entries.map {
+          x =>
+            var w = x.value * delta
+            if (x.i == x.j && w < 1.0) {
+             w = 1.0
+            }
 
-    //var mx= Skp1.toBlockMatrix.subtract(Sk.toBlockMatrix()).toCoordinateMatrix.entries.map(_.value.abs).max
-    for (i <- 0 to 10) { //mx >= math.pow(delta,k)) {
-      //println( "Ck -> "+"%.10f" format math.pow(delta,k))
-      Skp1 = new CoordinateMatrix(Sk.toBlockMatrix.multiply(matrixprob.toBlockMatrix.transpose).multiply(matrixprob.toBlockMatrix).toCoordinateMatrix.entries.map { x =>
-        val set = MatrixEntry(x.i, x.j, x.value * delta)
-        set
-      }).toBlockMatrix.add(In.toBlockMatrix).toCoordinateMatrix()
-      //mx = Skp1.toBlockMatrix.subtract(Sk.toBlockMatrix()).toCoordinateMatrix.entries.map(_.value.abs).max
-      Sk = Skp1
-      //println("MAX -> " + "%.10f" format mx)
+            MatrixEntry(x.i, x.j, w)
+        }).toBlockMatrix().toCoordinateMatrix()
+      tempMatrix = resultMatrix
+      logger.warn(s"[DEBUG] {$i}")
+      tempMatrix.entries.map {
+        case MatrixEntry(x, y, w) => (x, y, "%4.3f" format w)
+      }
+        .map(x => (x._1, (x._2, x._3))) // x, (y, w)
+        .join(vertices.map(x => (x._1, x._2._2))) // vertexIdx, xName
+        .map(x => (x._2._1._1, (x._2._2, x._2._1._2))) // y, (Name, w)
+        .join(vertices.map(x => (x._1, x._2._2))) // vertexIdy, yName
+        .map(x => (x._2._1._1, x._2._2, x._2._1._2)) // xName, yName, w
+        .filter(x => x._1 != x._2)
+        .sortBy(x => (x._1, x._3)).foreach(println)
     }
-    val result = Skp1.entries.map {
-      case MatrixEntry(x, y, j) => (x, y, "%4.3f" format j)
-    }.map(x => (x._1, (x._2, x._3))).join(indnode.map(x => (x._2, x._1.toLong)))
-      .map(x => (x._2._1._1, (x._2._1._2, x._2._2))).join(indnode.map(x => (x._2, x._1.toLong)))
-      .map(x => (x._2._1._2, x._2._2, x._2._1._1))
 
-    result.filter(_._3.toDouble < 1.0).saveAsTextFile("data/output/graphx/simrank_data")
+    // 13456
+    val result = resultMatrix.entries.map {
+      case MatrixEntry(x, y, w) => (x, y, "%4.3f" format w)
+    }
+      .map(x => (x._1, (x._2, x._3))) // x, (y, w)
+      .join(vertices.map(x => (x._1, x._2._2))) // vertexIdx, xName
+      .map(x => (x._2._1._1, (x._2._2, x._2._1._2))) // y, (Name, w)
+      .join(vertices.map(x => (x._1, x._2._2))) // vertexIdy, yName
+      .map(x => (x._2._1._1, x._2._2, x._2._1._2)) // xName, yName, w
+      .filter(x => x._1 != x._2)
+      .sortBy(x => (x._1, x._3))
+
+    logger.warn("[START] resultMatrix")
+    result.foreach(println)
+    //https://github.com/wanesta/Simrank
   }
 }
